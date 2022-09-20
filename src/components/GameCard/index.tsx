a88b99c1888dbcc2c3f37a1a6a31a4041e992d8d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-20 10:47:29 -0300

    chore: created game card component

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,36 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+  Text,
+} from 'react-native';
+
+import { styles } from './styles';
+
+import { THEME } from '../../theme';
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} adverts</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
