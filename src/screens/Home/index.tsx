83b3aa23cf3da10c66328c97530056aff1f15566
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-07-06 04:24:53 -0300

    chore: retrieved games data from api & stored in state/rendered it dynamically on interface

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,22 @@
+import { useState, useEffect } from 'react';
 import { View, Image, FlatList } from 'react-native';
+
+import { Heading } from '../../components/Heading';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { styles } from './styles';
 
-import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-
 import logoImg from '../../assets/logo-nlw-esports.png';
 
-import { GAMES } from '../../utils/games';
-
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.1.11:3333/games')
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -18,7 +25,7 @@ export function Home() {
         subtitle="Choose what game you want to play..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
