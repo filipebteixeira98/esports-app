146e592ec700b267c4282a39ff37bc2c8c7e9548
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-07-07 00:54:09 -0300

    feat: added functionality that allows users to go back to previous screen/received game information through route params & rendered it dynamically

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,13 +1,51 @@
+import { Image, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
 
 import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
 
+import { THEME } from '../../theme';
+
+import { GameParams } from '../../@types/navigation';
+
 export function Game() {
+  const route = useRoute();
+
+  const navigation = useNavigation();
+
+  const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image source={logoImg} style={styles.logo} />
+          <View style={styles.right} />
+        </View>
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+        <Heading title={game.title} subtitle="Log in and start playing!" />
+      </SafeAreaView>
     </Background>
   );
 }
