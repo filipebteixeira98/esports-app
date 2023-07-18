c88fd1a526d91ca2b4d77ca79f38009214e2c3d4
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-07-18 16:49:50 -0300

    chore: added padding to wrapper element which has container styles

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -6,16 +6,13 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingHorizontal: 32,
   },
   logo: {
     width: 214,
     height: 120,
     marginTop: 74,
     marginBottom: 48,
-  },
-  contentList: {
-    paddingLeft: 32,
-    paddingRight: 64,
   },
   button: {
     width: '100%',
